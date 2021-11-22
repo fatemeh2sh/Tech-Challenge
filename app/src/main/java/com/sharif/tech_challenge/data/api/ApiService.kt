92263168866c02c636421feb4452e3e8890501c3
sharif <fa.sharif.sh@gmail.com>
2021-11-22 11:21:24 +0330

    Fix Api

--- app/src/main/java/com/sharif/tech_challenge/data/api/ApiService.kt
+++ app/src/main/java/com/sharif/tech_challenge/data/api/ApiService.kt
@@ -1,13 +1,13 @@
 package com.sharif.tech_challenge.data.api
 
-import com.sharif.tech_challenge.data.model.ServiceModel
+import com.sharif.tech_challenge.data.model.CardModel
 import retrofit2.Response
-import retrofit2.http.*
+import retrofit2.http.GET
 
 interface ApiService {
 
     @GET("master/tempelate.json")
-    suspend fun allCards(): Response<ServiceModel>
+    suspend fun allCards(): Response<CardModel>
 
 
 }
