4969ad7305340d83cf98a5a670ab3b2e0955943a
sharif <fa.sharif.sh@gmail.com>
2021-11-22 12:48:47 +0330

    Add Enums

---
+++ app/src/main/java/com/sharif/tech_challenge/utils/Enums.kt
@@ -0,0 +1,7 @@
+package com.sharif.tech_challenge.utils
+
+enum class CodeType(type:Int) {
+    Picture(0),
+    Vibrate(1),
+    Sound(2)
+}
--- app/src/main/java/com/sharif/tech_challenge/utils/Util.kt
+++ app/src/main/java/com/sharif/tech_challenge/utils/Util.kt
@@ -9,6 +9,7 @@ import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.android.material.color.MaterialColors
+import com.sharif.tech_challenge.data.model.State
 
 fun getStringResources(context: Context, @StringRes resId: Int): String =
     context.resources.getString(resId)
@@ -34,6 +35,16 @@ fun closeKeyboard(activity: AppCompatActivity) {
         val manager: InputMethodManager? = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
         manager?.hideSoftInputFromWindow(view.windowToken, 0)
     }
+}
+
+fun getArrayState(): ArrayList<State> {
+
+    var lstState = arrayListOf<State>()
+    lstState.add(State("",2))
+    lstState.add(State("",3))
+    lstState.add(State("",4))
+
+    return lstState
 }
 
 
