071c3eef0dcc84142833ace80c09ac7d8cadb1c8
sharif <fa.sharif.sh@gmail.com>
2021-11-23 10:00:12 +0330

    Add new type

--- app/src/main/java/com/sharif/tech_challenge/utils/Enums.kt
+++ app/src/main/java/com/sharif/tech_challenge/utils/Enums.kt
@@ -1,13 +1,13 @@
 package com.sharif.tech_challenge.utils
 
-enum class CodeType(type:Int) {
+enum class CodeType(var type:Int) {
     Picture(0),
     Vibrate(1),
     Sound(2)
 }
 
-enum class ThemeType() {
-    sport,
-    art,
-    fun1
+enum class ThemeType(var type:String) {
+    Sport("sport"),
+    Art("art"),
+    Fun("fun")
 }
